75b2cdc71c626a6833a9d3fee24112ce3305b8e2
MauroFJunior <mauro.fernandesjr@sptech.school>
2026-08-30 12:22:02 -0300

    feat: backend initial commit

---
+++ soundstateBack/src/test/java/school/sptech/soundstate/SoundstateApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.soundstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
